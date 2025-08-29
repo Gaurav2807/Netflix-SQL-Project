fc4a0f2097eb7e59000fe90fd8a72814d9dc0890
Gaurav <gaurav2899gupta@gmail.com>
2025-08-30 01:06:40 +0530

    Table created with basic analytical queries

---
+++ Netflix_problem_statements_and_solutions.sql
@@ -0,0 +1,32 @@
+-- Netflix project
+DROP TABLE IF EXISTS Netflix;
+
+CREATE TABLE Netflix 
+(
+	show_id	VARCHAR(6),
+	content_type VARCHAR(10),
+	title VARCHAR(110),
+	director VARCHAR(210),
+	actors VARCHAR(780),
+	country	VARCHAR(130),
+	date_added VARCHAR(50),
+	release_year INT,
+	rating VARCHAR(10),
+	duration VARCHAR(10),
+	listed_in VARCHAR(80),
+	description VARCHAR(250)
+);
+
+-- All rows
+Select * from Netflix;
+
+-- Count of records
+Select 
+count(*) as total_content 
+from Netflix;
+
+-- Distinct content type
+Select 
+distinct(content_type) 
+from Netflix;
+
